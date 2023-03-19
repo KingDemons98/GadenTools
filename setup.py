a127f1cfbf1de84fb4dc2b4cd389e8fadbeef32f
KingDemons98 <79788577+KingDemons98@users.noreply.github.com>
2023-03-19 17:26:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from Cython.Build import cythonize
 setup(
     name='gadentools',
     ext_modules = cythonize(["gadentools/Simulation.pyx", "gadentools/Utils.pyx"]),
+    include_dirs=[numpy.get_include()],
     version='0.1.0',
     description='Python API to directly access GADEN simulation results, without depending on ROS.',
     author='Pepe Ojeda',
